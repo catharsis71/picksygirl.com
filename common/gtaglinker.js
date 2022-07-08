7170b37616c04fe059d6e7defae8a14a90d48ab8
cracky-chan <70603733+catharsis71@users.noreply.github.com>
2022-07-08 16:32:29 -0500

    .

--- common/gtaglinker.js
+++ common/gtaglinker.js
@@ -6,6 +6,6 @@ function gtag() {
 gtag('js', new Date());
 gtag('config', 'G-X378KKQJ60', {
 	'linker': {
-		'domains': ['cracky-chan.com', 'kamistik.com', 'crackychan.org', 'cracky-chan.tk', 'jemnezymy.com', 'crackychan.net', 'crackypedia.uk', 'dear-olivia.com', 'crackyhouse.com', 'angelesdelabismo.com', 'skyqueen.cc', 'cracky.win', 'catharsisqs4vi43ml3axj3x5akpimxislcidk7nwb6iol7pn4co4rqd.onion', 'cracky-chan.godaddysites.com', 'crackychan.bounceme.net']
+		'domains': ['cracky-chan.com', 'kamistik.com', 'crackychan.org', 'cracky-chan.tk', 'jemnezymy.com', 'crackychan.net', 'crackypedia.uk', 'dear-olivia.com', 'crackyhouse.com', 'angelesdelabismo.com', 'skyqueen.cc', 'cracky.win', 'catharsisqs4vi43ml3axj3x5akpimxislcidk7nwb6iol7pn4co4rqd.onion', 'cracky-chan.godaddysites.com', 'crackychan.bounceme.net', 'catharsis71.github.io']
 	}
 });
